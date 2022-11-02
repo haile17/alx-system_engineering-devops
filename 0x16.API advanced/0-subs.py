3021c598b8bed3611e7184bc295eb104385274eb
haile17 <hailefikere17@gmail.com>
2022-11-02 11:18:57 +0300

    how many subs?

---
+++ 0x16.API advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+function that queries that Reddit API and
+returns the number of subscriber count of a given subreddit
+"""
+from requests import get
+
+def number_of_subscribers(subreddit):
+    try:
+        h = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
+            headers={'user-agent'}, allow_redirects=False).json()
+        return i.get('data').get('subscribers') if not None else 0
+    except:
+        return 0
