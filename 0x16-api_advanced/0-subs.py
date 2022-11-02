6f8de4c8c96849754e31b4aec1e33448356e5072
haile17 <hailefikere17@gmail.com>
2022-11-02 11:40:52 +0300

    Top Ten

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,6 +9,6 @@ def number_of_subscribers(subreddit):
     try:
         h = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
             headers={'user-agent' : 'hAxr'}, allow_redirects=False).json()
-        return i.get('data').get('subscribers') if not None else 0
+        return h.get('data').get('subscribers') if not None else 0
     except:
         return 0
