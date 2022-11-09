900c72e46482fbdfbc93fe3da714ebb709415a38
haile17 <hailefikere17@gmail.com>
2022-11-09 06:54:23 +0000

    task 1

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# fix for nginx
+exec { 'change nginx ulimit':
+command  => 'echo ULIMIT="-n 2000" > /etc/default/nginx',
+path     => '/usr/bin',
+provider => 'shell'
+}
+
+exec { 'restart nginx':
+command  => 'service nginx restart',
+path     => '/usr/bin',
+provider => 'shell'
+}
